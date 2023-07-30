2a770f1f79cdd2d586f821f5f86b69c5061dc732
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-07-30 22:34:20 +0500

    half of work already done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,53 @@
 // Bismillah
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [step, setStep] = useState(1);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen w-screen items-center justify-center bg-[#8039A2] ">
+      <div className="flex w-[500px] justify-between bg-white p-4">
+        <div>
+          <button className="mb-3 block" onClick={() => setStep(1)}>
+            <span
+              className={`${
+                step >= 1 ? "bg-blue-500" : "bg-neutral-200"
+              } mr-2 rounded-full px-2 py-1 text-white`}
+            >
+              1
+            </span>
+            <span className="font-semibolt text-gray-200">Choose title</span>
+          </button>
+          <button className="mb-3 block" onClick={() => setStep(2)}>
+            <span
+              className={`${
+                step >= 2 ? "bg-blue-500" : "bg-neutral-200"
+              } mr-2 rounded-full px-2 py-1 text-white`}
+            >
+              2
+            </span>
+            <span className="font-semibolt text-gray-200">
+              Choose description
+            </span>
+          </button>
+          <button className="mb-3 block" onClick={() => setStep(3)}>
+            <span
+              className={`${
+                step >= 3 ? "bg-blue-500" : "bg-neutral-200"
+              } mr-2 rounded-full px-2 py-1 text-white`}
+            >
+              3
+            </span>
+            <span className="font-semibolt text-gray-200 ">Confirm data</span>
+          </button>
+        </div>
+        <div>
+          <h4 className="mb-3">Choose title content</h4>
+          <button className="bg-neutral-200 px-5 py-2">Submit title </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
